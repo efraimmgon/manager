14f8448130def0cc5486204c4e44b5c164a18951
efraimmgon <efraimmaugusto@gmail.com>
2017-11-07 07:52:13 -0300

    move views to own namespace

---
+++ src/cljs/manager/views.cljs
@@ -0,0 +1,76 @@
+(ns manager.views
+  (:require
+   [reagent.core :as r :refer [atom]]
+   [re-frame.core :as rf]
+   [manager.components :refer [base breadcrumbs thead tbody]]
+   [manager.events :refer [<sub]]
+   [manager.pages.features :as features]
+   [manager.pages.projects :as projects]
+   [manager.pages.tasks :as tasks]))
+
+(defn navbar []
+  (r/with-let [project (rf/subscribe [:project])]
+    [:nav.navbar.navbar-inverse
+     [:div.container-fluid
+      [:div.navbar-header
+       [:button.navbar-toggle.collapsed
+        {:aria-expanded "false",
+         :data-target "#bs-example-navbar-collapse-1",
+         :data-toggle "collapse",
+         :type "button"}
+        [:span.sr-only "Toggle navigation"]
+        [:span.icon-bar]
+        [:span.icon-bar]
+        [:span.icon-bar]]
+       [:a.navbar-brand {:href "/"} "Manager"]]
+      [:div#bs-example-navbar-collapse-1.collapse.navbar-collapse
+       [:ul.nav.navbar-nav.navbar-right
+        (when @project
+          [:li>a
+           {:href (str "/projects/" (:project-id @project) "/tasks/unfineshed")}
+           [:i.glyphicon.glyphicon-th-list]
+           " List pending tasks"])
+
+        [:li [:a {:href "/projects/new"} "Create project"]]]]]]))
+
+(def pages
+  {:home #'projects/projects-page
+   :new-project #'projects/new-project-page
+   :edit-project #'projects/edit-project-page
+   :project #'features/project-features-page
+   :project-tasks #'tasks/project-tasks-page
+   :new-feature #'features/new-feature-page
+   :edit-feature #'features/edit-feature-page
+   :feature-tasks #'tasks/feature-tasks-page
+   :new-task #'tasks/new-task-page
+   :edit-task #'tasks/edit-task-page})
+
+(defn modal [header body footer]
+  [:div
+   [:div.modal-dialog
+    [:div.modal-content
+     [:div.modal-header [:h3 header]]
+     [:div.modal-body body]
+     [:div.modal-footer
+      [:div.bootstrap-dialog-footer
+       footer]]]]
+   [:div.modal-backdrop.fade.in]])
+
+(defn error-modal []
+  (when-let [error (<sub [:error])]
+    [modal
+     "An error has occured"
+     [:div.alert.bg-danger
+      [:ul
+       (for [err error]
+         [:li (str err)])]]
+     [:div
+      [:button.btn.btn-sm.btn-danger
+       {:on-click #(rf/dispatch [:set-error nil])}
+       "OK"]]]))
+
+(defn page []
+  [:div
+   [error-modal]
+   [navbar]
+   [(pages @(rf/subscribe [:page]))]])
